9b2efacbd835f083724502132ad6342dd0266b8d
sanofar shajahan <sanofarshajahan16@gmail.com>
2025-10-13 21:21:29 +0200

    Initial commit for Wigell Gateway project

---
+++ src/main/java/com/wigell/gateway/WigellGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.wigell.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WigellGatewayApplication.class, args);
+    }
+}
